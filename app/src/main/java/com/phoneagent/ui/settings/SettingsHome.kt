62bde6a2b0e1521f420a76e36f319c75151794aa
xkx121029 <xkx121029@qq.com>
2026-09-22 21:01:03 +0800

    auto: happy_phone agent 2026-09-22 21:01:03

--- app/src/main/java/com/phoneagent/ui/settings/SettingsHome.kt
+++ app/src/main/java/com/phoneagent/ui/settings/SettingsHome.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.phoneagent.ui.components.LocalBottomNavClearance
 import com.phoneagent.ui.theme.AppSpacing
 import com.phoneagent.ui.icons.AppIcons
 
